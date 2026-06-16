872826fd441811071f2e55041df51346b2fb1269
Karan <karan03945@gmail.com>
2026-06-17 03:36:28 +0530

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { useAuthStore } from "@/store/useAuthStore";
 import Loading from "@/components/Loading";
 
 export default function Home() {
-  const router = useRouter();
+  const router = useRouter(); 
 
   const { user, checkAuth, isCheckingAuth } = useAuthStore();
 
